c08d7ef21049c068aa30fc1e45e3156565a0a1a4
William Gotterer <william.gotterer@gmail.com>
2021-09-22 13:41:01 -0400

    commit tree 0fd17efdf7b6b216cc9f9a7e75b16cab1683cd05

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Code your solution here
+
+
+function findMatching(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.toUpperCase() === name.toUpperCase() 
+    })
+}
+
+
+// findMatching(drivers, 'Bobby');
+
+
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function(driver){
+        return driver[0] === letters[0]
+    })
+
+}
+
+
+
+
+
+
+
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name === name
+    })
+
+}
+
+
+matchName(drivers, 'Bobby')
+
+
+
+
+
+
+
+
+
+
+
+
+// drivers.filter(findMatching(){
+    
+// };
+
+
+// function findMatching(drivers){
+//     return drivers === "Joe"
+// }
+
+
+// function findMatching (collection, cb){
+//     for(const truckman of collection){
+//         if(cb(truckman)){
+//             console.log(truckman.name);
+//         }
+//     }
+// }
+
+
+// findMatching(drivers, function(truckman){
+//     return truckman.name === "Joe"
+// });
+
+
+
